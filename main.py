6149026cb3b90bc1e6696dfc9e0bd1c3bcf93890
Matteo Gätzner <m.gaetzner@gmx.de>
2023-03-24 14:38:07 +0100

    file name sanitation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 from pathlib import Path
 
@@ -9,9 +9,16 @@ files_root_directory = Path("/home/www-matteogatzner/public/files/")
 url_root = "/file_server/"
 
 
+def sanitize_str(s: str) -> str:
+    keepcharacters = (" ", ".", "_")
+    return "".join(c for c in s if c.isalnum() or c in keepcharacters).rstrip()
+
+
 @app.get(url_root + "{file_name}", response_class=FileResponse)
 async def main(file_name: str):
-    file_path = files_root_directory / file_name
+    file_path = files_root_directory / sanitize_str(file_name)
     if not file_path.exists():
-        raise FileNotFoundError(f"File with file name {file_name} not found!")
+        raise HTTPException(
+            status_code=404, detail=f"File with file name {file_name} not found!"
+        )
     return file_path
